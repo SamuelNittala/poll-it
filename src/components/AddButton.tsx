23e6306a46cba06902982bd77e7ceac02115a58e
Samuel Nittala <nittalasamuel7@gmail.com>
2023-01-11 12:44:04 +0530

    feat: modify AddButton for storybook

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,11 +1,22 @@
 import { useDescription } from "@ts-react/form";
 import React from "react";
 
-export const AddButton = ( { addAnswer } : { addAnswer : any}) => {
-  const { label } = useDescription();
+export const AddButton = ({
+  addAnswer,
+  label,
+}: {
+  addAnswer: () => void;
+  label: string;
+}) => {
   return (
     <div className="m-3 w-full">
-      <button type="button" onClick={addAnswer} className="text-sm bg-red-100 p-2 rounded-md">{label}</button>
+      <button
+        type="button"
+        onClick={addAnswer}
+        className="text-sm bg-red-100 p-2 rounded-md"
+      >
+        {label}
+      </button>
     </div>
   );
 };
