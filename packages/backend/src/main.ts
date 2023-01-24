04339eeafcf8f3bd760a7b139bcd5d201480f203
Samuel Nittala <nittalasamuel7@gmail.com>
2023-01-24 21:31:40 +0530

    add jwt authentication

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import router from './router';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
+import { protect } from './modules/auth';
+
+dotenv.config()
 
 const app = express();
 const PORT = 3001;
@@ -9,11 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
+
+app.use('/api', protect, router);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
